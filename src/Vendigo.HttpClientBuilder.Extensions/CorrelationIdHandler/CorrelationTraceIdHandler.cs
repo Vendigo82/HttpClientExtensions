19284f247dd34386746733e7a178d43541b339a8
Vendigo82 <Vendigo82@yandex.ru>
2021-05-01 10:59:36 +0800

    Расширение для добавления PollyPolicies и записи TraceId как CorrelationId

---
+++ src/Vendigo.HttpClientBuilder.Extensions/CorrelationIdHandler/CorrelationTraceIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vendigo.HttpClientBuilder.CorrelationIdHandler
+{
+    public class CorrelationTraceIdHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private string _correlationHeaderName;
+
+        public CorrelationTraceIdHandler(string correlationHeaderName, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+            _correlationHeaderName = correlationHeaderName ?? throw new ArgumentNullException(nameof(correlationHeaderName));
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var traceId = _httpContextAccessor.HttpContext.TraceIdentifier;
+            request.Headers.Add(_correlationHeaderName, traceId);
+
+            return base.SendAsync(request, cancellationToken);
+        }
+
+    }
+}
---
+++ src/Vendigo.HttpClientBuilder.Extensions/PollyPolices/PollyPolicesExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Configuration;
+using Polly;
+using Polly.Extensions.Http;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class PollyPolicesExtensions
+    {
+        public const string PoliciesConfigurationSectionName = "HttpPolicies";
+
+        /// <summary>
+        /// Register Http polly policies from configuration
+        /// </summary>
+        /// <see cref="https://rehansaeed.com/optimally-configuring-asp-net-core-httpclientfactory/"/>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <param name="configurationSectionName"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddConfigurationHttpClientPolicies(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            //var section = configuration.GetSection(configurationSectionName);
+            //services.Configure<PolicyOptions>(configuration);
+            var policyOptions = configuration.Get<PolicyOptions>() ?? new PolicyOptions();
+
+            var policyRegistry = services.AddPolicyRegistry();
+
+            policyRegistry.Add(
+                PolicyName.HttpRetry,
+                HttpPolicyExtensions
+                    .HandleTransientHttpError()
+                    .WaitAndRetryAsync(
+                        policyOptions.HttpRetry.Count,
+                        retryAttempt => TimeSpan.FromSeconds(Math.Pow(policyOptions.HttpRetry.BackoffPower, retryAttempt))));
+
+            policyRegistry.Add(
+                PolicyName.HttpCircuitBreaker,
+                HttpPolicyExtensions
+                    .HandleTransientHttpError()
+                    .CircuitBreakerAsync(
+                        handledEventsAllowedBeforeBreaking: policyOptions.HttpCircuitBreaker.ExceptionsAllowedBeforeBreaking,
+                        durationOfBreak: policyOptions.HttpCircuitBreaker.DurationOfBreak));
+
+            return services;
+        }
+
+        public static class PolicyName
+        {
+            public const string HttpCircuitBreaker = nameof(HttpCircuitBreaker);
+            public const string HttpRetry = nameof(HttpRetry);
+        }
+
+        private class PolicyOptions
+        {
+            public CircuitBreakerPolicyOptions HttpCircuitBreaker { get; set; } = new CircuitBreakerPolicyOptions();
+            public RetryPolicyOptions HttpRetry { get; set; } = new RetryPolicyOptions();
+        }
+
+        private class CircuitBreakerPolicyOptions
+        {
+            public TimeSpan DurationOfBreak { get; set; } = TimeSpan.FromSeconds(30);
+            public int ExceptionsAllowedBeforeBreaking { get; set; } = 12;
+        }
+
+        private class RetryPolicyOptions
+        {
+            public int Count { get; set; } = 3;
+            public int BackoffPower { get; set; } = 2;
+        }
+    }
+}
